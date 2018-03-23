eaafd241fb1f300f2cdfdc8787ace0b9a58c6557
Simon Norra <simon.norra@codecentric.de>
2018-03-23 16:27:49 +0100

    Added global error message to insert book page for adding another book with already present isbn but with different details

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/InsertBookController.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/controller/InsertBookController.java
@@ -3,17 +3,22 @@ package de.codecentric.psd.worblehat.web.controller;
 import de.codecentric.psd.worblehat.domain.Book;
 import de.codecentric.psd.worblehat.domain.BookService;
 import de.codecentric.psd.worblehat.web.formdata.BookDataFormData;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.thymeleaf.messageresolver.StandardMessageResolver;
 
 import javax.validation.Valid;
+
 import java.util.Optional;
 
 /**
@@ -23,38 +28,42 @@ import java.util.Optional;
 @RequestMapping("/insertBooks")
 public class InsertBookController {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(InsertBookController.class);
+    private static final Logger LOG = LoggerFactory
+            .getLogger(InsertBookController.class);
 
-	private BookService bookService;
+    private BookService bookService;
 
-	@Autowired
-	public InsertBookController(BookService bookService) {
-		this.bookService = bookService;
-	}
+    @Autowired
+    public InsertBookController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
-	@RequestMapping(method = RequestMethod.GET)
-	public void setupForm(ModelMap modelMap) {
-		modelMap.put("bookDataFormData", new BookDataFormData());
-	}
+    @RequestMapping(method = RequestMethod.GET)
+    public void setupForm(ModelMap modelMap) {
+        modelMap.put("bookDataFormData", new BookDataFormData());
+    }
 
-	@RequestMapping(method = RequestMethod.POST)
-	public String processSubmit(@ModelAttribute("bookDataFormData") @Valid BookDataFormData bookDataFormData,
-			BindingResult result) {
+    @RequestMapping(method = RequestMethod.POST)
+    public String processSubmit(@ModelAttribute("bookDataFormData") @Valid BookDataFormData bookDataFormData,
+                                BindingResult result) {
 
-		if (result.hasErrors()) {
-			return "insertBooks";
-		} else {
-			Optional<Book> book = bookService.createBook(bookDataFormData.getTitle(), bookDataFormData.getAuthor(),
-					bookDataFormData.getEdition(), bookDataFormData.getIsbn(),
-					Integer.parseInt(bookDataFormData.getYearOfPublication()));
-			if (book.isPresent()) {
-			    LOG.info("new book instance is created: " + book.get());
+        if (result.hasErrors()) {
+            return "insertBooks";
+        } else {
+            Optional<Book> book = bookService.createBook(bookDataFormData.getTitle(), bookDataFormData.getAuthor(),
+                    bookDataFormData.getEdition(), bookDataFormData.getIsbn(),
+                    Integer.parseInt(bookDataFormData.getYearOfPublication()));
+            if (book.isPresent()) {
+                LOG.info("new book instance is created: " + book.get());
             } else {
-			    LOG.debug("failed to create new book with: "+bookDataFormData.toString());
+                LOG.debug("failed to create new book with: " + bookDataFormData.toString());
+                result.addError(new ObjectError("copyDoesNotMatch",
+                        new String[]{"de.codecentric.psd.worblehat.web.insertBook.copyDoesNotMatch"},
+                        new Object[]{bookDataFormData.getIsbn()}, "DEFAULT"));
+                return "insertBooks";
             }
-			return "redirect:bookList";
-		}
-	}
+            return "redirect:bookList";
+        }
+    }
 
 }
