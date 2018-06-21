d7a41c39c54616b24d1e173ec4b4cec4ae8d797c
franknoelle <frannoel@EMUEPC803.global.europipe.local>
2018-06-21 16:06:08 +0200

    WB 117

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/validation/ISBNConstraintValidator.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/validation/ISBNConstraintValidator.java
@@ -1,10 +1,10 @@
 package de.codecentric.psd.worblehat.web.validation;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.validator.routines.ISBNValidator;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.validator.routines.ISBNValidator;
 
 public class ISBNConstraintValidator implements ConstraintValidator<ISBN, String> {
 
@@ -16,7 +16,10 @@ public class ISBNConstraintValidator implements ConstraintValidator<ISBN, String
 	public boolean isValid(String value, ConstraintValidatorContext context) {
 		// Don't validate null, empty and blank strings, since these are validated by @NotNull, @NotEmpty and @NotBlank
 		if(StringUtils.isNotBlank(value)) {
-			return ISBNValidator.getInstance().isValidISBN10(value);
+			ISBNValidator validator = ISBNValidator.getInstance();
+			final boolean isValidISBN10 = validator.isValidISBN10(value);
+			final boolean isValidISBN13 = validator.isValidISBN13(value);
+			return isValidISBN10 || isValidISBN13 ;
 		}
 		return true;
 	}
